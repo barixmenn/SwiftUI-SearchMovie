db3acc348a8f2abff3c765dd8786882022b04487
Barış <93044357+barixmenn@users.noreply.github.com>
2022-06-11 22:17:44 +0300

    Add files via upload

---
+++ FilmAramaSwiftUI/App/FilmAramaSwiftUIApp.swift
@@ -0,0 +1,16 @@
+//
+//  FilmAramaSwiftUIApp.swift
+//  FilmAramaSwiftUI
+//
+//
+
+import SwiftUI
+
+@main
+struct FilmAramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FilmListeView()
+        }
+    }
+}
